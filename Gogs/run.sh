43bd272ec8c51f8f7df4cbe67f083943622c1c3f
Ciel <imwithye@gmail.com>
2017-03-12 15:36:01 +0800

    better docker ps

--- Gogs/run.sh
+++ Gogs/run.sh
@@ -2,4 +2,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run -d --name gogs-data -v /$DIR/data:/data busybox
 docker run -d --name gogs -p 18181:22 -p 18180:3000 --volumes-from gogs-data gogs/gogs
-docker ps
+echo
+docker ps -a -f "name=gogs*"
