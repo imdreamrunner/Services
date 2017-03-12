985217481ab40b21a5e9014549bcad12492fac8e
Ciel <imwithye@gmail.com>
2017-03-12 15:29:25 +0800

    remvoe

--- Nextcloud/run.sh
+++
@@ -1,5 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-docker run -d --name nextcloud-data -v /$DIR/data:/var/www/html busybox
-docker run -d --name nextcloud -p 18182:9000 --volumes-from nextcloud-data indiehosters/nextcloud
-docker ps
